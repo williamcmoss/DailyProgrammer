2ea4d651c23fe02add14c3a3054bd7f348aa20d2
williamcmoss <43445451+williamcmoss@users.noreply.github.com>
2019-11-12 22:55:16 -0600

    fixed issue with easy smorse, started on intermediate smorse

--- [2019-08-07] Challenge #380 [Intermediate] Smooshed Morse Code 2.py
+++ [2019-08-07] Challenge #380 [Intermediate] Smooshed Morse Code 2.py
@@ -3,7 +3,10 @@ morseLetters = morseCode.split()
 latinLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 morseDict = {}
 for i in range(0,26):
-    morseDict[latinLetters[i]]=morseLetters[i]
+    morseDict[morseLetters[i]]=latinLetters[i]
 
-def smorse(word):
-    return ''.join([morseDict[letter] for letter in word])
+def smalpha(smorse):
+    if smorse == "":
+        return ''
+    if smorse[0:2] in morseDict:
+        return morseDict[smorse[0:2]]+smalpha(smorse[2:])
