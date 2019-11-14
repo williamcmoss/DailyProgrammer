b7504856521eab2c285b2f5877a709b18c8a7091
williamcmoss <43445451+williamcmoss@users.noreply.github.com>
2019-11-13 21:27:27 -0600

    Trying to solve intermediate smooshed Morse Code in Prolog

--- [2019-08-07] Challenge #380 [Intermediate] Smooshed Morse Code 2.py
+++ [2019-08-07] Challenge #380 [Intermediate] Smooshed Morse Code 2.py
@@ -3,10 +3,13 @@ morseLetters = morseCode.split()
 latinLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 morseDict = {}
 for i in range(0,26):
-    morseDict[morseLetters[i]]=latinLetters[i]
+    print 'morse(\''+str(latinLetters[i])+'\',\''+str(morseLetters[i])+'\').'
 
 def smalpha(smorse):
     if smorse == "":
         return ''
     if smorse[0:2] in morseDict:
         return morseDict[smorse[0:2]]+smalpha(smorse[2:])
+    if smorse[0:2] in morseDict:
+        return morseDict[smorse[0:2]]+smalpha(smorse[2:])
+
