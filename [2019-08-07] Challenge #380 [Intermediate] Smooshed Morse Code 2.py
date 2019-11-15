b6278e1bc9f12b0e7f079120c722d7f8e1cc0f04
williamcmoss <43445451+williamcmoss@users.noreply.github.com>
2019-11-14 20:26:54 -0600

    working on intermediate version in prolog still

--- [2019-08-07] Challenge #380 [Intermediate] Smooshed Morse Code 2.py
+++ [2019-08-07] Challenge #380 [Intermediate] Smooshed Morse Code 2.py
@@ -3,7 +3,7 @@ morseLetters = morseCode.split()
 latinLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 morseDict = {}
 for i in range(0,26):
-    print 'morse(\''+str(latinLetters[i])+'\',\''+str(morseLetters[i])+'\').'
+    print 'morse(\"'+str(latinLetters[i])+'\",\"'+str(morseLetters[i])+'\").'
 
 def smalpha(smorse):
     if smorse == "":
